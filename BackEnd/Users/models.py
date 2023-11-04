7b68ddceeb19738b9febd5902322707768408d66
Dekodac <Dekodac@gmail.com>
2023-11-04 12:36:15 -0400

    added custom user model with abstract user and registered in admin file

--- BackEnd/Users/models.py
+++ BackEnd/Users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+from datetime import date
+class User(AbstractUser):
+  username = models.CharField(max_length = 50, blank = True, null = True, unique = True)
+  email = models.EmailField(_('email address'), unique = True)
+  phone_no = models.CharField(max_length = 10)
+  USERNAME_FIELD = 'email'
+  REQUIRED_FIELDS = ['username']
 
-# Create your models here.
+
+  def __str__(self):
+      return "{}".format(self.email)
+
