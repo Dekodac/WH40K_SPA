bd2ffc6baf995a29b8a06084aa8a58eba3853e24
Dekodac <Dekodac@gmail.com>
2023-11-05 19:54:06 -0500

    created React project, and made template on main app jsx, had issues with images being uploaded to db, had to install Pillow, also made changes to my Characters gender field to be choices

--- FrontEnd/WH40K_SPA_React_Proj/src/App.jsx
+++ FrontEnd/WH40K_SPA_React_Proj/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from 'react';
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [show, setShow] = useState(true);
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/characters/')
+      .then(response => {
+        setCharacters(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching characters:', error);
+      });
+  }, []);
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {show ? <h1>Who?</h1> : <p>Bubs</p>}
+      <button onClick={() => setShow(!show)}>Who do I love?</button>
+      <h1>Character Information</h1>
+      <ul>
+        {characters.map(character => (
+        <>
+        <li key={character.id}>
+          {console.log(character.char_img)}
+          {character.char_name} - {character.race} - {character.gender} - 
+        </li>
+          <img src={character.char_img} width={100} height={150} />
+        </>
+  ))}
+
+</ul>
+
+    </div>
+  );
 }
 
 export default App
