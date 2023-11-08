105b820aba7ba776fd0bc727ab3c9c92d019de0d
Dekodac <Dekodac@gmail.com>
2023-11-08 14:55:39 -0500

    edited cypress test and my navbar boostrap component to use Link.

--- FrontEnd/WH40K_SPA_React_Proj/src/components/NavBar.jsx
+++ FrontEnd/WH40K_SPA_React_Proj/src/components/NavBar.jsx
@@ -2,8 +2,10 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
+  
   return (
     <div >
     <Navbar >
@@ -12,13 +14,13 @@ function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/" id='bolder' className='detail-one'>Home</Nav.Link>
-            <Nav.Link href="/about/" id='bolder' className='detail-one'>About</Nav.Link>
-            <Nav.Link href="/gallery/" id='bolder' className='detail-one'>Gallery</Nav.Link>
-            <Nav.Link href="/commissions/" id='bolder' className='detail-one'>Commissions</Nav.Link>
+            <Nav.Link as={Link} to="/" id='bolder' className='detail-one'>Home</Nav.Link>
+            <Nav.Link as={Link} to="/about/" id='bolder' className='detail-one'>About</Nav.Link>
+            <Nav.Link as={Link} to="/gallery/" id='bolder' className='detail-one'>Gallery</Nav.Link>
+            <Nav.Link as={Link} to="/commissions/" id='bolder' className='detail-one'>Commissions</Nav.Link>
             <NavDropdown title="More" id="basic-nav-dropdown" className='detail-one'>
-              <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Sign up</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/login/">Login</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/sign_up/">Sign up</NavDropdown.Item>
               {/* <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item> */}
               {/* <NavDropdown.Divider /> */}
               {/* <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
